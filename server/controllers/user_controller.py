4ab1394f3027516a2136e3a09990ec7e3ff32354
Ryan Holt <ryan.holt.rmh@gmail.com>
2020-02-27 12:12:26 -0700

    Added user dto and updated get user route

--- server/controllers/user_controller.py
+++ server/controllers/user_controller.py
@@ -1,17 +1,22 @@
-from flask import jsonify
-from flask_restplus import Resource
+from flask_restplus import Resource, fields
 
 from server.api.restplus import api
 from server.models.user import User
 
 ns = api.namespace('users', description='Operations related to users')
 
+user_dto = api.model('user', {
+    'email': fields.String(required=True, description='user email address'),
+    'username': fields.String(required=True, description='user username'),
+})
+
 
 @ns.route('/')
 class UserList(Resource):
+    @ns.marshal_list_with(user_dto)
     def get(self):
         """
         Gets all registered users
         """
         results = User.query.all()
-        return jsonify(results)
+        return results
