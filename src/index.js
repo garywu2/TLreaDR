199bdd35eb5ea48b0d4963ed93a676d0b7532daf
Jofred Cayabyab <39203355+JCayabyab@users.noreply.github.com>
2020-03-09 13:51:32 -0600

    install styled components (#10)
    add basic themes
    import fonts

--- src/index.js
+++ src/index.js
@@ -1,18 +1,20 @@
-import 'babel-polyfill';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { store, history } from './store';
-import { routes } from './routes';
-import { ConnectedRouter } from 'connected-react-router';
-import './assets/styles/style';
+import "babel-polyfill";
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { store, history } from "./store";
+import { routes } from "./routes";
+import { ConnectedRouter } from "connected-react-router";
+import "./assets/styles/main.css";
+import { ThemeProvider } from "styled-components";
+import theme from "./assets/styles/theme";
 
 // render the main component
 ReactDOM.render(
   <Provider store={store}>
-    <ConnectedRouter history={history}>
-      {routes}
-    </ConnectedRouter>
+    <ThemeProvider theme={theme}>
+      <ConnectedRouter history={history}>{routes}</ConnectedRouter>
+    </ThemeProvider>
   </Provider>,
-  document.getElementById('app')
+  document.getElementById("app")
 );
