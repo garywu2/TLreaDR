638adea4cb20909b3768dfef3d598aa71487b568
Tyler Lam <41935461+tylerlam1@users.noreply.github.com>
2020-03-11 14:21:35 -0600

    Hotfix/navigation bar (#22)
    * change anchor tags to link
    
    * changes to the way state is handled in navbar
    
    * implement sign out feature
    
    * added padding to sign-in & changed links
    
    * got rid of unused library
    
    * fixed route
    
    * rename button to SignOutButton to ensure consistency
    
    * rename button to SignOutButton to ensure consistency

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,6 +3,9 @@ import logo from "../assets/TLreaDR-logo.png";
 import styled from "styled-components";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import { Link, useHistory } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux'; 
+import { LOGOUT_USER } from "../actions/types";
 
 const NavbarWrapper = styled.div`
   background-color: #ef3e36;
@@ -12,12 +15,23 @@ const NavbarWrapper = styled.div`
   padding: 10px 25px;
 `;
 
-const Button = styled.a`
+const SignOutButton = styled.a`
   color: #ffffff;
   font-size: 18px;
   font-family: "Montserrat", "sans-serif";
   text-decoration: none;
+  cursor: pointer;
+  padding: 15px 10px;
 `;
+
+const SignInButton = styled(Link)`
+  color: #ffffff;
+  font-size: 18px;
+  font-family: "Montserrat", "sans-serif";
+  text-decoration: none;
+  cursor: pointer;
+  padding: 15px 10px;
+`
 
 const LogoImage = styled.img`
   height: 50px;
@@ -34,7 +48,7 @@ const CategoryButton = styled.div`
   float: left;
 `;
 
-const Anchor = styled.a`
+const PageReference = styled(Link)`
   padding: 14px 16px;
   color: white;
   text-align: center;
@@ -52,26 +66,35 @@ const Search = styled.div`
 `
 
 const Navbar = () => {
+  const userAccount = useSelector(state => state.user);
+  const dispatch = useDispatch();
+  const history = useHistory();
+
+  const handleLogout = () => {
+      dispatch({type:LOGOUT_USER});
+      history.push('/');
+  };
+
   return (
     <React.Fragment>
       <NavbarWrapper>
         <div></div>
         <LogoImage src={logo} alt="TLreaDR" />
-        <Button href="/sign-in">Sign In</Button>
+        {!userAccount ? <SignInButton to="/sign-in">Sign In</SignInButton> : <SignOutButton onClick={handleLogout}>Log Out</SignOutButton>}
       </NavbarWrapper>
       <SubheaderWrapper>
         <div>
           <CategoryButton>
-            <Anchor href="/home">Home</Anchor>
+            <PageReference to="/">Home</PageReference>
           </CategoryButton>
           <CategoryButton>
-            <Anchor href="/News">News</Anchor>
+            <PageReference to="/News">News</PageReference>
           </CategoryButton>
           <CategoryButton>
-            <Anchor href="/Sports">Sports</Anchor>
+            <PageReference to="/Sports">Sports</PageReference>
           </CategoryButton>
           <CategoryButton>
-            <Anchor href="/Lifestyle">Lifestyle</Anchor>
+            <PageReference to="/Lifestyle">Lifestyle</PageReference>
           </CategoryButton>
         </div>
         <Search>
