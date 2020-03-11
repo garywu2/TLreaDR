26cc1a09fb32d773918b145b351182468bfb7dc2
garywu2 <gary.wu2@ucalgary.ca>
2020-03-11 12:04:44 -0600

    Adding some parts of sign in page

---
+++ src/components/SignInPage/index.jsx
@@ -0,0 +1,16 @@
+import React from "React";
+import SignInForm from "./SignInForm";
+import { useDispatch, useSelector } from "react-redux";
+
+const SignInPage = () => {
+
+  // later write code to post to API
+
+  return (
+    <div>
+      <SigninPage></SigninPage>
+    </div>
+  );
+};
+
+export default SignInPage;
