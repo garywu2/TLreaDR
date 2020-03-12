9e5f59b58100a7dafdc4841961188d5472544fb7
derek-braun <derek.braun.99@gmail.com>
2020-03-12 13:25:57 -0600

    Add key term search for news service

--- server/controllers/news_controller.py
+++ server/controllers/news_controller.py
@@ -6,12 +6,8 @@ from server.api.restplus import api
 
 ns = api.namespace('news', description='Operations related to news service')
 
-category_parser = reqparse.RequestParser()
-category_parser.add_argument('name', required=True, type=str, help='name of category', location='json')
-
-url = ('http://newsapi.org/v2/top-headlines?'
-       'country=us&'
-       'apiKey=b511c877204a447b8ae3d01af4b875b3')
+news_search_parser = reqparse.RequestParser()
+news_search_parser.add_argument('keyword', required=True, type=str, help='search term', location='json')
 
 @ns.route('')
 class CategoryCollection(Resource):
@@ -19,5 +15,22 @@ class CategoryCollection(Resource):
         """
         Gets all top news articles
         """
+        url = ('http://newsapi.org/v2/top-headlines?'
+               'country=us&'
+               'apiKey=b511c877204a447b8ae3d01af4b875b3')
+        results = requests.get(url)
+        return results.json(), 200
+
+    @api.expect(news_search_parser)
+    def post(self):
+        """
+        Search by key word
+        """
+        args = news_search_parser.parse_args()
+        url = ('http://newsapi.org/v2/everything?'
+               'q=' + args['keyword'] + '&'
+               'from=2020-03-12&'
+               'sortBy=popularity&'
+               'apiKey=b511c877204a447b8ae3d01af4b875b3')
         results = requests.get(url)
         return results.json(), 200
