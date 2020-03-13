1013147608b3de7673e17d850a8b400af4358402
Jofred Cayabyab <39203355+JCayabyab@users.noreply.github.com>
2020-03-13 15:06:13 -0600

    add signup validation (#38)
    - prevent empty signup fields

--- src/components/styled/FormInput.jsx
+++ src/components/styled/FormInput.jsx
@@ -1,27 +1,51 @@
 import styled from "styled-components";
-import React from "react";
+import React, { useState } from "react";
+
+const Labels = styled.div`
+  margin-left: 5px;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const ErrorMessage = styled.label`
+  color: red;
+  visibility: ${({ showError }) => (showError ? "visible" : "hidden")};
+`;
 
 const Input = styled.input`
   width: 100%;
-`
+`;
 
 export default props => {
-  const { type, hasError, label, handleInputChange, ...inputProps } = props;
+  const {
+    type,
+    hasError,
+    errorMessage,
+    label,
+    handleInputChange,
+    triedSubmit,
+    ...inputProps
+  } = props;
+  const [touched, setTouched] = useState(false);
 
   const updateInput = e =>
     handleInputChange && handleInputChange(e.target.value);
 
+  const showError = hasError && (touched || triedSubmit);
+
   return (
-    <div style={{margin: "10px 0"}}>
-      <div style={{ marginLeft: "5px" }}>
+    <div style={{ margin: "10px 0" }}>
+      <Labels>
         <label className={props}>{label}</label>
-      </div>
+        <ErrorMessage showError={showError}>{errorMessage}</ErrorMessage>
+      </Labels>
       <div>
         <Input
           {...inputProps}
           type={type || "text"}
+          onBlur={() => setTouched(true)}
           onChange={updateInput}
-          className={props.hasError ? "error" : ""}
+          className={showError ? "error" : ""}
         />
       </div>
     </div>
