ffd397429afd6950bec078c46ab8b9cc9379175b
Tyler Lam <41935461+tylerlam1@users.noreply.github.com>
2020-03-21 19:27:31 -0600

    Feature/nav bar dynamic subcategories (#35)
    * refactor subheader to use a list
    
    * change list to contain objects
    
    * fix category links
    
    * wrap subheader categories
    
    * spacing
    
    * catch up to master
    
    * fix routes
    
    * fix category name
    
    * added calls to API to get categories
    
    * fix error handling

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,13 @@
 import { combineReducers } from "redux";
 import userReducer from "./userReducer";
 import postsReducer from "./postsReducer";
+import categoriesReducer from "./categoriesReducer";
 import { connectRouter } from "connected-react-router";
 
 export const rootReducer = history =>
   combineReducers({
     user: userReducer,
     posts: postsReducer,
+    categories: categoriesReducer,
     router: connectRouter(history)
   });
