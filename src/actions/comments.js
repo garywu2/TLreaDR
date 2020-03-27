58f01fcb07c9b2e5656c3a893b2ec30d07e38430
Jofred Cayabyab <jofred.cayabyab1@ucalgary.ca>
2020-03-27 01:28:46 -0600

    finish upload comment action

--- src/actions/comments.js
+++ src/actions/comments.js
@@ -5,24 +5,34 @@ import { FETCH_COMMENTS, UPLOAD_COMMENTS } from "./types";
 export const getCommentsByPostUuid = async post_uuid => {
   const response = await axios.get(`${config.endpoint}comments/${post_uuid}`);
 
-  if(response.status !== 200) {
+  if (response.status !== 200) {
     throw "getPosts failed with error code " + response.status;
   }
 
   return { type: "GET_POST", comments: response.data };
 };
 
-export const uploadComment = async (comment) => {
-  const params = { comment };
+export const postComment = async (
+  commentText,
+  authorUuid,
+  postUuid,
+  parentId
+) => {
+  const body = {
+    text: commentText,
+    author_uuid: authorUuid,
+    post_uuid: postUuid
+  };
+  if (parentId) {
+    body.parent_id = parentId;
+  }
 
   // put comment into database
-  const response = await axios.get(config.endpoint + "comment", {
-    params
-  });
+  const response = await axios.post(config.endpoint + "comments", body);
 
-  if(response.status !== 200) {
+  if (response.status !== 200) {
     throw "comment failed with error code " + response.status;
   }
 
-  return {type: UPLOAD_COMMENTS, user: response.data}
+  return { type: "POST_COMMENT", response: response.data };
 };
