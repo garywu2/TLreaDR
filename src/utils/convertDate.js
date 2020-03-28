5865e37f0b6cfb57be2a544b5e353b70212d6d6c
Jofred Cayabyab <jofred.cayabyab1@ucalgary.ca>
2020-03-28 00:27:35 -0600

    update convertDate

--- src/utils/convertDate.js
+++ src/utils/convertDate.js
@@ -1,3 +1,21 @@
 export default function convertDate(flaskDate) {
-  return flaskDate.slice(0, 10).replace(/-/g, "/")
+  console.log(flaskDate);
+
+  // convert time to AM/PM
+  let time = flaskDate.slice(11, 16);
+  console.log(time);
+  // convert to number and check if greater than 12
+  let hours = +time.slice(0,2)
+  let minutes = +time.slice(3,5)
+  let ending = "AM";
+  if(hours > 12) {
+    ending = "PM";
+    hours -= 12;
+  }
+  console.log(hours, minutes);
+
+  let string = flaskDate.slice(0, 10).replace(/-/g, "/");
+  string += ` ${hours}:${minutes} ${ending}`
+
+  return string;
 }
