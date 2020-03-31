a9fe5a9d88695d87f7cbb477c6c17c4169f96c6f
harsohailbrar <harsohail.brar1@gmail.com>
2020-03-30 18:32:12 -0600

    fix votes ordering due to single column of votes

--- server/parsers/server_parsers.py
+++ server/parsers/server_parsers.py
@@ -2,3 +2,7 @@ from flask_restplus import reqparse
 
 post_get_parser = reqparse.RequestParser()
 post_get_parser.add_argument('user_uuid', required=False, type=str, help='user that requested get')
+
+user_login_parser = reqparse.RequestParser()
+user_login_parser.add_argument('username', required=True, type=str, help='username of user logging in')
+user_login_parser.add_argument('password', required=True, type=str, help='password of user logging in')
