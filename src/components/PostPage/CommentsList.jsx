64d19e6822c9934530a08d9b41e2feea0824e9e2
Jofred Cayabyab <jofred.cayabyab1@ucalgary.ca>
2020-03-31 18:13:38 -0600

    add sign in blurb if user is not logged in

--- src/components/PostPage/CommentsList.jsx
+++ src/components/PostPage/CommentsList.jsx
@@ -2,10 +2,23 @@ import React from "react";
 import styled from "styled-components";
 import CommentThread from "./CommentThread";
 import CommentForm from "./CommentForm";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 const Title = styled.h2`
   color: white;
   margin: 10px 0px;
+`;
+
+const SignInText = styled.div`
+  color: white;
+  text-align: center;
+  width: 100%;
+  margin: 10px auto;
+`;
+
+const SignInLink = styled(Link)`
+  text-decoration: underline;
 `;
 
 export default function CommentsList({
@@ -14,13 +27,31 @@ export default function CommentsList({
   handleEditSubmit,
   handleDelete
 }) {
+  const user = useSelector(state => state.user);
+
+  const renderForm = () => {
+    if (user) {
+      return (
+        <React.Fragment>
+          <Title>Post a comment</Title>
+          <CommentForm
+            handleSubmit={commentText => handleCommentSubmit(commentText, [])}
+            placeholder="Write a comment here..."
+          ></CommentForm>
+        </React.Fragment>
+      );
+    } else {
+      return (
+        <SignInText>
+          <SignInLink to="/sign-in">Sign in</SignInLink> to post a comment!
+        </SignInText>
+      );
+    }
+  };
+
   return (
     <div>
-      <Title>Post a comment</Title>
-      <CommentForm
-        handleSubmit={commentText => handleCommentSubmit(commentText, [])}
-        placeholder="Write a comment here..."
-      ></CommentForm>
+      {renderForm()}
       {comments ? (
         <div>
           <Title>Comments</Title>
